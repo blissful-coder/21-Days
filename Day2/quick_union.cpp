00430ed56bed99889c23db489ae3bb382a89f100
i323878 <anurag.negi@sap.com>
2022-02-21 20:47:35 +0530

    code(day2): quick union algorithm

--- Day2/quick_union.cpp
+++ Day2/quick_union.cpp
@@ -5,5 +5,15 @@ using namespace std;
 static const int N = 10000;
 int main()
 { 
-    //TODO
+     int i, j, p, q, id[N];
+    for(i=0; i<N; i++) id[i] = i;
+
+    while (cin >> p >> q){
+        cout<<p<<"-"<<q<<endl;
+        for (i = p; i != id[i]; i = id[i]) ;
+        for (j = q; j != id[j]; j = id[j]) ;
+        if (i == j) continue;
+        id[i] = j;
+        cout << " " << p << " " << q << endl;
+    }
 }
